240828a1eb16eb7c565345edbdd2edb209a1382e
fahadamin2288 <mrfahad2288@gmail.com>
2025-01-18 13:21:32 +0500

    Add files via upload

---
+++ Add numbers.py
@@ -0,0 +1,12 @@
+
+# This program take two arguments and returns the sum.
+
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+number1 = int(input("Please enter the first argument: "))
+number2 = int(input("Please enter the second argument: "))
+result = add_numbers(number1, number2)
+print(result)
