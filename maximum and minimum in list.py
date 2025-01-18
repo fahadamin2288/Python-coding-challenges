13a12e725e007c499e761e1b15849eab9a3fdbee
Fahad Amin <mrfahad2288@gmail.com>
2025-01-18 18:17:47 +0500

    Update and rename maximum in list.py to maximum and minimum in list.py

---
+++ maximum and minimum in list.py
@@ -0,0 +1,21 @@
+# This python program takes a list of numbers as input and returns the maximum number in the list.
+
+
+def find_max_and_num(num):
+    max = num[0]
+    min = num[0]
+    if (len(num)) > 1:
+        for i in range(1, len(num)):
+            if num[i] > max:
+                max = num[i]
+            if num[i] < min:
+                min = num[i]
+        return f"Maximum element is: {max}\nMinimum element is: {min}"
+    else:
+        return 0
+
+
+
+list = [3, 5, 4, 1, 9]
+list = [3]
+print(find_max_and_num(list))
